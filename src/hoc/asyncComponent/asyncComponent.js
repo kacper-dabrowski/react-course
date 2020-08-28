8301178203b5260fb4ef45038c2201b43be344a2
Kacper <kdtw301@gmail.com>
2020-08-28 16:56:51 +0200

    Course sync - office pc

---
+++ src/hoc/asyncComponent/asyncComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+const asyncComponent = (importComponent) => {
+  return class extends Component {
+    state = { component: null };
+    componentDidMount() {
+      importComponent().then((cmp) =>
+        this.setState({ component: cmp.default })
+      );
+    }
+    render() {
+      const { component: Component } = this.state;
+      return Component ? <Component {...this.props} /> : null;
+    }
+  };
+};
+export default asyncComponent;
